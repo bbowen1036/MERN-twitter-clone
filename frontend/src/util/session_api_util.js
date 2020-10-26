9ff6463034be9704609ad6dcb9e33bd6b1803d86
brianbowen <bbowen1036@gmail.com>
2020-10-25 22:03:44 -0700

    MERN twitter hw

---
+++ frontend/src/util/session_api_util.js
@@ -0,0 +1,20 @@
+//setAuthToken method to either set or delete the common header 
+// dependent on whether the token is passed into our method:
+
+import axios from 'axios';
+
+export const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token;
+  } else {
+    delete axios.defaults.headers.common['Authorization'];
+  }
+};
+
+export const signup = (userData) => {
+  return axios.post('/api/users/register', userData);
+};
+
+export const login = (userData) => {
+  return axios.post('/api/users/login', userData);
+};
